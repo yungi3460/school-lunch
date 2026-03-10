69d3f5b7e735056eed19736f959d1f1394c9990d
yungi3460 <yungigim20@gmail.com>
2026-03-10 23:30:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -105,6 +105,6 @@ def tomorrow():
     menu = get_menu(date)
     return render_template_string(HTML, menu=menu)
 
-app.run(host="0.0.0.0", port=5000)
 if __name__ == "__main__":
+
     app.run(host="0.0.0.0", port=10000)
